22a1be7978d68c4c1be8de4496d5a8e7c2a15d24
Hendrik Werner <hendrik.to@gmail.com>
2016-05-20 14:42:47 +0200

    Add a main class stub.

---
+++ assignment13/Assignment13.java
@@ -0,0 +1,16 @@
+package assignment13;
+
+/**
+ *
+ * @author Hendrik Werner // s4549775
+ */
+public class Assignment13 {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+    }
+
+}
