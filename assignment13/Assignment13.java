315829fb769c9123e5737bf8b8490d4d8034357c
Hendrik Werner <hendrik.to@gmail.com>
2016-05-20 21:41:41 +0200

    Add a method to test the file finder.

--- assignment13/Assignment13.java
+++ assignment13/Assignment13.java
@@ -4,8 +4,6 @@ import filefinder.FileFinder;
 import java.io.IOException;
 import java.util.logging.Level;
 import java.util.logging.Logger;
-import mergesort.MergeSort;
-import mergesort.Sorter;
 
 /**
  *
@@ -17,26 +15,14 @@ public class Assignment13 {
      * @param args the command line arguments
      */
     public static void main(String[] args) {
-//        try {
-//            new FileFinder("C:\\").findFile("FileFinder.java");
-//        } catch (IOException ex) {
-//            Logger.getLogger(Assignment13.class.getName()).log(Level.SEVERE, null, ex);
-//        }
+        testFileFinder();
+    }
 
-        int[] ints = new int[10];
-        for (int i = 0; i < 10; i++) {
-            ints[i] = (int) (Math.random() * 128);
-        }
-//        MergeSort.sort(ints);
-        Thread t = new Thread(new Sorter(ints));
-        t.start();
+    private static void testFileFinder() {
         try {
-            t.join();
-        } catch (InterruptedException ex) {
+            new FileFinder("C:\\").findFile("FileFinder.java");
+        } catch (IOException ex) {
             Logger.getLogger(Assignment13.class.getName()).log(Level.SEVERE, null, ex);
-        }
-        for (int i = 0; i < 10; i++) {
-            System.out.println(i + ": " + ints[i]);
         }
     }
 
