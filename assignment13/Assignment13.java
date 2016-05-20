da438997975c147f008c93ee1bc1ce8eb3351784
Hendrik Werner <hendrik.to@gmail.com>
2016-05-20 20:10:59 +0200

    Add a setter for the array.

--- assignment13/Assignment13.java
+++ assignment13/Assignment13.java
@@ -1,5 +1,12 @@
 package assignment13;
 
+import filefinder.FileFinder;
+import java.io.IOException;
+import java.util.logging.Level;
+import java.util.logging.Logger;
+import mergesort.MergeSort;
+import mergesort.Sorter;
+
 /**
  *
  * @author Hendrik Werner // s4549775
@@ -10,7 +17,27 @@ public class Assignment13 {
      * @param args the command line arguments
      */
     public static void main(String[] args) {
-        // TODO code application logic here
+//        try {
+//            new FileFinder("C:\\").findFile("FileFinder.java");
+//        } catch (IOException ex) {
+//            Logger.getLogger(Assignment13.class.getName()).log(Level.SEVERE, null, ex);
+//        }
+
+        int[] ints = new int[10];
+        for (int i = 0; i < 10; i++) {
+            ints[i] = (int) (Math.random() * 128);
+        }
+//        MergeSort.sort(ints);
+        Thread t = new Thread(new Sorter(ints));
+        t.start();
+        try {
+            t.join();
+        } catch (InterruptedException ex) {
+            Logger.getLogger(Assignment13.class.getName()).log(Level.SEVERE, null, ex);
+        }
+        for (int i = 0; i < 10; i++) {
+            System.out.println(i + ": " + ints[i]);
+        }
     }
 
 }
--- mergesort/Sorter.java
+++ mergesort/Sorter.java
@@ -1,6 +1,8 @@
 package mergesort;
 
 import java.util.Arrays;
+import java.util.logging.Level;
+import java.util.logging.Logger;
 
 /**
  * Sort an array of integers.
@@ -8,6 +10,8 @@ import java.util.Arrays;
  * @author Hendrik Werner // s4549775
  */
 public class Sorter implements Runnable {
+
+    private static final int THRESHOLD = 1000;
 
     private final int[] array;
 
@@ -17,9 +21,35 @@ public class Sorter implements Runnable {
 
     @Override
     public void run() {
-        if (array.length > 1) {
-            // todo
+        if (array.length > THRESHOLD) {
+            Sorter s1 = new Sorter(
+                    Arrays.copyOf(array, array.length / 2)
+            );
+            Sorter s2 = new Sorter(
+                    Arrays.copyOfRange(array, array.length / 2, array.length)
+            );
+            Thread t1 = new Thread(s1);
+            Thread t2 = new Thread(s2);
+            t1.start();
+            t2.start();
+            try {
+                t1.join();
+                t2.join();
+            } catch (InterruptedException ex) {
+                Logger.getLogger(Sorter.class.getName()).log(Level.SEVERE, null, ex);
+            }
+            System.arraycopy(s1.getArray(), 0, array, 0, s1.getArray().length);
+            System.arraycopy(s2.getArray(), 0, array, s1.getArray().length, s2.getArray().length);
+        } else {
+            MergeSort.sort(array);
         }
+    }
+
+    /**
+     * @return the array
+     */
+    private int[] getArray() {
+        return array;
     }
 
 }
