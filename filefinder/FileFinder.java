6feae4168fcf04ad318e60c68d3b2229e7767560
Hendrik Werner <hendrik.to@gmail.com>
2016-05-20 16:25:59 +0200

    Use a folder searcher to search for files.

--- filefinder/FileFinder.java
+++ filefinder/FileFinder.java
@@ -21,20 +21,7 @@ public class FileFinder {
     }
 
     public void findFile(String file) {
-        find(rootDir, file);
-    }
-
-    private void find(File rootDir, String fileName) {
-        File[] files = rootDir.listFiles();
-        if (files != null) {
-            for (File file : files) {
-                if (file.getName().equals(fileName)) {
-                    System.out.println("Found at: " + file.getAbsolutePath());
-                } else if (file.isDirectory()) {
-                    find(file, fileName);
-                }
-            }
-        }
+        new Thread(new FolderSearcher(rootDir, file)).start();
     }
 
 }
